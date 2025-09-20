45cc9aead1857442dad63e0065bdb66092b8e65d
hadid imtiyaz <hadidimtiyaz0@gmail.com>
2025-09-20 18:45:35 +0530

    average temperature

---
+++ assignment14.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+int main() {
+    std::vector<int> temps = {
+        55, 62, 68, 74, 59, 45, 41, 58, 60, 67, 65, 78, 82, 88, 91,
+        92, 90, 93, 87, 80, 78, 79, 72, 68, 61, 59
+    };
+
+    int hot = 0, pleasant = 0, cold = 0;
+    int sum = 0;
+
+    for (int temp : temps) {
+        std::cout << "Temperature: " << temp << " - ";
+        if (temp >= 85) {
+            std::cout << "Hot day\n";
+            hot++;
+        } else if (temp >= 60) {
+            std::cout << "Pleasant day\n";
+            pleasant++;
+        } else {
+            std::cout << "Cold day\n";
+            cold++;
+        }
+        sum += temp;
+    }
+
+    double average = static_cast<double>(sum) / temps.size();
+
+    std::cout << "\nNumber of hot days: " << hot << std::endl;
+    std::cout << "Number of pleasant days: " << pleasant << std::endl;
+    std::cout << "Number of cold days: " << cold << std::endl;
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Average temperature: " << average << std::endl;
+
+    return 0;
+}
