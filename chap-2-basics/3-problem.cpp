ef3031a98c279e9fa651d8471b65465db4da2b53
hadid imtiyaz <hadidimtiyaz0@gmail.com>
2025-12-27 17:23:54 +0530

    3-problem

---
+++ chap-2-basics/3-problem.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << (number = 10) << endl
+         << (number *= 2) << endl
+         << --number << endl;
+
+    return 0;
+}
