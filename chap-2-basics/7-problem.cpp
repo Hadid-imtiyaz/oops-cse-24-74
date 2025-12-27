f6f90eb1c6b4ffe3829284733ced1cd7633b4a8b
hadid imtiyaz <hadidimtiyaz0@gmail.com>
2025-12-27 17:29:49 +0530

    7-problem

---
+++ chap-2-basics/7-problem.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double celsiusTemperature;
+
+    cout << "Enter celsius temperature:";
+    cin >> celsiusTemperature;
+
+    cout << celsiusTemperature << " in Fahrenheit system is "
+         << celsiusTemperature / 5 * 9 + 32;
+
+    return 0;
+}
